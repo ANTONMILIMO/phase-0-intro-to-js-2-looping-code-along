69ac60fba5cb19e93daa5b25221bf09a628eb028
Antony Milimo <antonymilimo2017@gmail.com>
2025-03-25 23:24:03 +0300

    for loop and while loop practice

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let customMessages = [];
+  
+    for (let i = 0; i < names.length; i += 1) {
+      customMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return customMessages;
+  }
+  
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  
+  // while loop
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number -= 1;
+    }
+  }
+  
+  console.log(countDown(10));
